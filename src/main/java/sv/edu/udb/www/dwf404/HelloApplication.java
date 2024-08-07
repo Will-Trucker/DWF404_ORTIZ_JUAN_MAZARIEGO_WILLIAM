3166fd60e6f111619d6084ea02a746eb152febec
William-Orella <willitomarley@outlook.es>
2024-08-07 11:00:03 -0600

    Inicialización de Repositorio

---
+++ src/main/java/sv/edu/udb/www/dwf404/HelloApplication.java
@@ -0,0 +1,9 @@
+package sv.edu.udb.www.dwf404;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
